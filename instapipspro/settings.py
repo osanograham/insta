c0e7b943413635e7631538b06e39291aa04fface
osanograham <108458179+osanograham@users.noreply.github.com>
2022-08-13 13:52:30 +0300

    Update settings.py

--- instapipspro/settings.py
+++ instapipspro/settings.py
@@ -131,12 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'instapipspro/static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
+
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = 'static/'
 django_heroku.settings(locals())
 
 # Default primary key field type
